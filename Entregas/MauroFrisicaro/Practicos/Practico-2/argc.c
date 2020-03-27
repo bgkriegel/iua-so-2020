7ac32ac8bfc56b88a36471bd82372e8905d3b5f4
MauroFrisicaro <maurofrisicaro22@gmail.com>
2020-03-27 07:42:44 -0300

    Practico 2.1

---
+++ Entregas/MauroFrisicaro/Practicos/Practico-2/argc.c
@@ -0,0 +1,17 @@
+/* 1) Escriba un programa que muestre en orden todos los argumentos
+   pasados por linea de comando. Para ello argumente la funcion main()
+   de su programa de la siguiente manera: int main(int argc, char *argv[])
+   De esta manera "argc" contiene la cantidad de elementos de la linea
+   de comando, mientras que "argv[]" es un array de char que contiene
+   cada uno de los componentes.
+   Tip: Solo debe recorrer el arreglo argv imprimiendo su contenido. */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+
+	for (int i = 0; i < argc; i++ ) {
+	printf("%s\n", argv[i]);
+	}
+
+}
