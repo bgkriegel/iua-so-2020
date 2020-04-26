75352723599a53d35a2c3fd5ac6642291cf501d0
Matias <matiasarriete@hotmail.com>
2020-04-26 15:14:45 -0300

    Practico 3

---
+++ Entregas/Arriete/Practico3/Ejercicio1.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+	int fd;
+	int i =1;
+
+	while(i > 0){
+
+		fd = open("archivoAbrir.txt", O_RDONLY);
+
+		if (fd == -1)
+		{
+			printf("El archivo se mantiene cerrado, abra el archivo 'archivoAbrir'\n");
+			sleep(1);
+		} else {
+			printf("El archivo se ha abierto en la interacion: %d\n", i);
+			exit(1);
+		}
+
+		i++;
+
+
+	}
+
+
+	return 0;
+
+}
