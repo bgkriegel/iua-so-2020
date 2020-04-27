73d95937f68458736f12f3d1b1135c6b58c3d995
rdesouza <53132202+r-desouza@users.noreply.github.com>
2020-04-27 04:13:48 -0300

    1.2

--- Entregas/deSouzaRafael/Practico-1/2.c
+++ Entregas/deSouzaRafael/Practico-1/2.c
@@ -2,3 +2,15 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+int main() {
+    int pid;
+
+    pid = fork();
+
+    if (pid == 0) {
+        printf("I'm the son.\n");
+    } else {
+        printf("This is the father.\n");
+    }
+    return 0;
+}
