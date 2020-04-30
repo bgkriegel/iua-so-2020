5061bc844cda38bc812b45976d1ff20ae41ba2e3
JazminBustos <jaazbustos@gmail.com>
2020-04-30 05:57:07 +0200

    punto2

--- Entregas/JazminBustos/Practico3/punto2.c
+++ Entregas/JazminBustos/Practico3/punto2.c
@@ -3,34 +3,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_BYTES 1 
+#define MAX_BYTES 1
 
-int main(){
+int main()
+{
 
 	int fd, i = 1;
 	char buff;
 
-	while(i > 0){
+	while (i > 0) {
 
 
 
 
-		fd = open("txt.txt", O_RDONLY|O_CREAT);
+		fd = open("txt.txt", O_RDONLY | O_CREAT);
 
-		if (fd == -1)
-		{
+		if (fd == -1) {
 			printf("ERROR, NO SE PUDO ABRIR \n");
 			exit(1);
 		} else {
-			read(fd,&buff,MAX_BYTES);
-			if (buff == '1')
-			{
+			read(fd, &buff, MAX_BYTES);
+			if (buff == '1') {
 				printf("ESTA CORRIENDO\n");
 				sleep(1);
 			} else {
-				if (buff == '0')
-				{
-					printf(" Numero de interacion:%d\n",i);
+				if (buff == '0') {
+					printf(" Numero de interacion:%d\n", i);
 
 					exit(1);
 				} else {
@@ -39,7 +37,7 @@ int main(){
 					sleep(1);
 				}
 
-				
+
 
 			}
 
